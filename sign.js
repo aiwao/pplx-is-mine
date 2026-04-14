5e2b21d01a16a2fbf47d30e3d44626a54355939b
aiwao <233898056+aiwao@users.noreply.github.com>
2026-04-15 02:59:41 +0900

    ci: sign firefox addon

---
+++ sign.js
@@ -0,0 +1,24 @@
+const webExt = require("web-ext").default || require("web-ext")
+
+async function main() {
+  const apiKey = process.env.AMO_JWT_ISSUER
+  const apiSecret = process.env.AMO_JWT_SECRET
+
+  if (!apiKey || !apiSecret) {
+    throw new Error('Missing AMO credentials')
+  }
+
+  await webExt.cmd.sign({
+    apiKey,
+    apiSecret,
+    sourceDir: "./src",
+    artifactsDir: "./build",
+    channel: "listed",
+    userAgentString: "pplx-is-mine-release-bot",
+  })
+}
+
+main().catch((err) => {
+  console.error(err)
+  process.exit(1)
+})
